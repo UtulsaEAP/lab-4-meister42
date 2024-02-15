ade18c54d5f88ea724e38dff81a98c3335c6decf
meister42 <hgh585@utulsa.edu>
2024-02-15 17:10:26 -0600

    problem 4

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -1,13 +1,20 @@
 """
 Complete the following python code to reverse the string entered by the user.
 
-Name: 
-Lab Time:
+Name: Henry Holman
+Lab Time: Thursday @ 2pm
 """
 
 def reverse_string():
     # YOUR CODE HERE
-    print()
+    string = str(input())
+    rev = ""
+    while string not in ["d", "done", "Done"]:
+        rev = ""
+        for letter in string:
+            rev = letter + rev
+        print(rev)
+        string = str(input())
 
 if __name__ == "__main__":
     reverse_string()
